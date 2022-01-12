b736e941a638088b1ea0698d9489fe0d9791e656
Vladimir <vladimir.d.babichev@gmail.com>
2022-01-12 08:23:09 +0000

    Fixed bootstrapping

--- scripts/bootstrap-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Source common functions
-# shellcheck source=/dev/null
-source "$(dirname "$(realpath "$0")")/common.sh"
-
-# 1. Install ArgoCD
-# 1. Install Apps
--- scripts/common.sh
+++ scripts/common.sh
@@ -5,15 +5,15 @@ root_dir=$(git rev-parse --show-toplevel)
 export ROOT_DIR=$ROOT_DIR
 
 info() {
-    echo -e "$1"
+    echo -e "[INFO] $1"
 }
 
 err() {
-    echo -e "$1"
+    echo -e "[ERROR] $1"
 }
 
 fail() {
     [ -n "$2" ] && err_code=$2 || err_code=1
-    echo -e "$1"
+    err "$1"
     exit $err_code
 }
